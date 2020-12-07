7b5caaecc7c7a6a6a3563c6462a733e538e0a6b9
Tenzin-Norden <nordentenzin29@gmail.com>
2020-12-06 20:23:19 -0500

    f init

--- pcap/src/main/java/com/github/username/App.java
+++ pcap/src/main/java/com/github/username/App.java
@@ -1,5 +1,9 @@
 // App.java
 package com.github.username;
+
+import java.io.File;
+import java.io.*; // Import the FileWriter class
+
 import java.io.IOException;
 import java.net.Inet4Address;
 import com.sun.jna.Platform;
@@ -30,18 +34,26 @@ public class App {
     static boolean first_packet_time = false;
     static boolean last_packet_time = false;
 
+    static String toWrite = "";
+
     public static void main(String[] args) throws PcapNativeException, NotOpenException {
         System.out.println("Let's start analysis ");
-        // New code below here
+        // // New code below here
 
         final PcapHandle handle;
 
         handle = Pcaps.openOffline("small.pcap");
 
+        System.out.println("handle");
         System.out.println(handle);
 
         PacketListener listener = new PacketListener() {
             public void gotPacket(Packet packet) {
+
+                toWrite += packet + "\n";
+                // System.out.println(packet);
+
+                String lines[] = packet.getPayload().toString().split("\\r?\\n");
 
                 if (first_packet_time == false) {
                     first_pack_time = (double) handle.getTimestamp().getTime();
@@ -58,19 +70,35 @@ public class App {
 
                 if (packet.get(UdpPacket.class) != null) {
                     TCP_number = TCP_number + 1;
+
+                    try {
+                        // System.out.println("Source: " + lines[11].substring(19));
+                        // System.out.println("Dest " + lines[12].substring(24));
+                        // System.out.println(lines[33].split(" ")[4]);
+                        // System.out.println(lines[34].split(" ")[4]);
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        // e.printStackTrace();
+                    }
                 }
 
             }
         };
 
         try {
-
             int maxPackets = -1;
             handle.loop(maxPackets, listener);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
+        try {
+            File myfile = new File("filename.txt");
+            FileWriter myWriter = new FileWriter("filename.txt");
+            myWriter.write(toWrite);
+            myWriter.close();
+        } catch (Exception e) {
+
+        }
         double total_time = last_pack_time - first_pack_time;
         total_time = total_time / 1000.0;
 
